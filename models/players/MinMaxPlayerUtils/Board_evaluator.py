7d6f3aedad974864a6c04c08779f04a0dccf4f34
RenanIOliveira <renanioliveira@dcc.ufrj.br>
2019-10-23 19:49:26 -0300

    removendo documentacao desnecessaria

--- models/players/MinMaxPlayerUtils/Board_evaluator.py
+++ models/players/MinMaxPlayerUtils/Board_evaluator.py
@@ -1,20 +1,14 @@
 
 
-def EvaluateBoard(board, heuristics_list, *weights_list):
-    """Computes the distance from the origin to the point (x, y)
-    
-    :param board: the point's x-coordinate
-    :param heuristics_list: the point's y-coordinate
-    :return: number. the distance from (0, 0) to the point (x, y)
-     """
+def evaluateBoard(board, heuristics_list, *weights_list):
     if len(weights_list) != 0:
         weights = weights_list[0]
-        return EvaluateBoardWithWeights(board, heuristics_list, weights)
+        return evaluateBoardWithWeights(board, heuristics_list, weights)
     else:
-        return EvaluateBoardWithoutWeights(board, heuristics_list)
-    
+        return evaluateBoardWithoutWeights(board, heuristics_list)
 
-def EvaluateBoardWithoutWeights(board, heuristics_list):
+
+def evaluateBoardWithoutWeights(board, heuristics_list):
     board_value = 0
     for heuristic in heuristics_list:
         board_value += heuristic(board)
@@ -22,14 +16,12 @@ def EvaluateBoardWithoutWeights(board, heuristics_list):
     return board_value
 
 
-def EvaluateBoardWithWeights(board, heuristics_list, weights):
-    
+def evaluateBoardWithWeights(board, heuristics_list, weights):
+
     if len(heuristics_list) != len(weights):
         raise Exception('heuristics_list and weights_list have diferent sizes')
     board_value = 0
     for i in range(0, len(heuristics_list)):
         board_value += weights[i] * heuristics_list[i](board)
-    
+
     return board_value
-
-
