6c6cea87f786ef2d46a56d6a9f3e34792ae174cf
RenanIOliveira <renanioliveira@dcc.ufrj.br>
2019-10-30 11:51:10 -0300

    implementando heuristica de estabilidade

--- models/players/MinMaxPlayerUtils/MinMax.py
+++ models/players/MinMaxPlayerUtils/MinMax.py
@@ -8,14 +8,17 @@ INF = 0x3f3f3f3f
 MAXIMUM_SEARCH_DEPTH = 3
 MIN_TIME = datetime.datetime.now()
 
+
 def calculatePlay(Board, mypieces):
     global MIN_TIME
     MIN_TIME = datetime.datetime.now()
-    move = minMax(Board, 0, MAXIMUM_SEARCH_DEPTH, True, mypieces, -INF, INF, MIN_TIME)
+    move = minMax(Board, 0, MAXIMUM_SEARCH_DEPTH,
+                  True, mypieces, -INF, INF)
     print datetime.datetime.now() - MIN_TIME
     return move
 
-def minMax(Board, current_depth, max_depth, IsMaxNode, myPieces, alpha, beta, initial_time):
+
+def minMax(Board, current_depth, max_depth, IsMaxNode, myPieces, alpha, beta):
     whiteMoves = Board.valid_moves(Board.WHITE)
     blackMoves = Board.valid_moves(Board.BLACK)
     numberOfWhiteMoves = whiteMoves.__len__()
@@ -27,17 +30,19 @@ def minMax(Board, current_depth, max_depth, IsMaxNode, myPieces, alpha, beta, in
         if(evaluateBoard(Board, myPieces, [heuristics.piecesDiference]) > 0):
             return INF
         # if we lose or draw
-        if(evaluateBoard(Board, myPieces, [heuristics.piecesDiference]) <= 0):
+        if(evaluateBoard(Board, myPieces, [heuristics.piecesDiference]) < 0):
             return -INF
+        if(evaluateBoard(Board, myPieces, [heuristics.piecesDiference]) == 0):
+            return -INF+1
 
     if(current_depth == max_depth):
-        current_time = datetime.datetime.now()
-        step_time_difference = current_time - initial_time
-        time_difference = current_time - MIN_TIME
-        if ((time_difference + step_time_difference).total_seconds() >= 3):
-            return evaluateBoard(Board, myPieces, [heuristics.piecesDiference])
-        else:
-            return minMax(Board, current_depth, max_depth+1, IsMaxNode, myPieces, alpha, beta, datetime.datetime.now())
+        # current_time = datetime.datetime.now()
+        # step_time_difference = current_time - initial_time
+        # time_difference = current_time - MIN_TIME
+        # if ((time_difference + step_time_difference).total_seconds() >= 3):
+        return evaluateBoard(Board, myPieces, [heuristics.piecesDiference])
+        # else:
+        #     return minMax(Board, current_depth, max_depth+1, IsMaxNode, myPieces, alpha, beta, datetime.datetime.now())
 
     if(IsMaxNode):
         # if is maxnode myPieces plays
@@ -48,7 +53,7 @@ def minMax(Board, current_depth, max_depth, IsMaxNode, myPieces, alpha, beta, in
 
         # if there are no plays pass the turn
         if(moves.__len__() == 0):
-            return minMax(Board, current_depth+1, max_depth, not IsMaxNode, myPieces, alpha, beta, datetime.datetime.now())
+            return minMax(Board, current_depth+1, max_depth, not IsMaxNode, myPieces, alpha, beta)
 
         # calculate the best move
         maximum = -INF
@@ -57,7 +62,7 @@ def minMax(Board, current_depth, max_depth, IsMaxNode, myPieces, alpha, beta, in
             new_board = copy.deepcopy(Board)
             new_board.play(move, Board.WHITE)
             current_child_value = minMax(
-                new_board, current_depth+1, max_depth, not IsMaxNode, myPieces, alpha, beta, datetime.datetime.now())
+                new_board, current_depth+1, max_depth, not IsMaxNode, myPieces, alpha, beta)
             if(maximum <= current_child_value):
                 maximum = current_child_value
                 best_move = move
@@ -77,7 +82,7 @@ def minMax(Board, current_depth, max_depth, IsMaxNode, myPieces, alpha, beta, in
 
         # if there are no plays pass the turn
         if(moves.__len__() == 0):
-            return minMax(Board, current_depth+1, max_depth, not IsMaxNode, myPieces, alpha, beta, datetime.datetime.now())
+            return minMax(Board, current_depth+1, max_depth, not IsMaxNode, myPieces, alpha, beta)
 
         # calculate the best move
         minimum = INF
@@ -85,11 +90,11 @@ def minMax(Board, current_depth, max_depth, IsMaxNode, myPieces, alpha, beta, in
             new_board = copy.deepcopy(Board)
             new_board.play(move, Board.BLACK)
             current_child_value = minMax(
-                new_board, current_depth+1, max_depth, not IsMaxNode, myPieces, alpha, beta, datetime.datetime.now())
+                new_board, current_depth+1, max_depth, not IsMaxNode, myPieces, alpha, beta)
             if(current_child_value <= minimum):
                 minimum = current_child_value
                 best_move = move
-            beta = min(beta,minimum)
+            beta = min(beta, minimum)
             if(alpha >= beta):
                 break
         if(current_depth != 0):
--- models/players/MinMaxPlayerUtils/heuristics.py
+++ models/players/MinMaxPlayerUtils/heuristics.py
@@ -5,6 +5,7 @@ def piecesDiference(board, myPieces):
     else:
         return black - white
 
+
 def mobility(board, myPieces):
     numberOfWhiteMoves = board.valid_moves(board.WHITE).__len__()
     numberOfBlackMoves = board.valid_moves(board.BLACK).__len__()
@@ -16,3 +17,51 @@ def mobility(board, myPieces):
             return (numberOfBlackMoves - numberOfWhiteMoves)/(numberOfWhiteMoves + numberOfBlackMoves)
     else:
         return 0
+
+
+corners = [[1, 1], [1, 8], [8, 1], [8, 8]]
+UP, DOWN, LEFT, RIGHT = [-1, 0], [1, 0], [0, -1], [0, 1]
+
+
+def stability(board, myPieces):
+    global corners
+    for corner in corners:
+        calculateStables(board, myPieces, corner)
+
+
+stable_pieces = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0], ]
+
+
+def calculateStables(board, myPieces, initial_position):
+    global UP, DOWN, LEFT, RIGHT
+    global stable_pieces
+
+    if initial_position == corners[0]:
+        horizontal_step = RIGHT
+        vertical_step = DOWN
+     if initial_position==corners[1]:
+        horizontal_step = LEFT
+        vertical_step = DOWN
+    if initial_position==corners[2]:
+        horizontal_step = RIGHT
+        vertical_step = UP
+    if initial_position==corners[2]:
+        horizontal_step = LEFT
+        vertical_step = UP
+
+    n_stables,n_unstables,n_semi_stables = 0
+    current_position = initial_position
+    for i in range(1,9):
+
+        for j in range(1,9):
+
+
+    return number_of_stables
